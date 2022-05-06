f21ff6f4036faabd5f101a8cd495876309152b1b
yeehongchew <64217618+yeehongchew@users.noreply.github.com>
2022-05-06 18:35:42 +0800

    Add files via upload

--- milestone1/main.c
+++ milestone1/main.c
@@ -95,9 +95,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+	/*  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
 	  HAL_Delay(1000);
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+	  HAL_Delay(1000); */
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
